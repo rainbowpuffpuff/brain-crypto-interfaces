a6900ed5b4afaa9fef2f3d1a9969685aef743fab
rainbowpuffpuff <83903147+rainbowpuffpuff@users.noreply.github.com>
2024-04-14 10:55:54 +0200

    Add files via upload

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,87 +1,89 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-// Useful for debugging. Remove when deploying to a live network.
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
+contract YourContract is Ownable, ReentrancyGuard {
+    struct Submission {
+        address submitter;
+        bytes32 mediaURIHash;   // Hash of the media URI
+        bytes32 eegDataHash;    // Hash of the EEG data
+        uint256 tokenReward;
+        bool isPaid;
+        bool eegDataSubmitted;  // Indicates if EEG data has been submitted
+    }
 
-/**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
- * @author BuidlGuidl
- */
-contract YourContract {
-	// State Variables
-	address public immutable owner;
-	string public greeting = "Building Unstoppable Apps!!!";
-	bool public premium = false;
-	uint256 public totalCounter = 0;
-	mapping(address => uint) public userGreetingCounter;
+    mapping(uint256 => Submission) public submissions;
+    uint256 public submissionCount = 1; // Start counting submissions from 1
 
-	// Events: a way to emit log statements from smart contract that can be listened to by external parties
-	event GreetingChange(
-		address indexed greetingSetter,
-		string newGreeting,
-		bool premium,
-		uint256 value
-	);
+    event MediaSent(uint256 indexed submissionId, bytes32 mediaURIHash, address indexed recipient);
+    event EEGDataSubmitted(uint256 indexed submissionId, bytes32 eegDataHash);
+    event EtherDeposited(address indexed sender, uint256 amount);
+    event PaymentMade(uint256 indexed submissionId, uint256 amount);
 
-	// Constructor: Called once on contract deployment
-	// Check packages/hardhat/deploy/00_deploy_your_contract.ts
-	constructor(address _owner) {
-		owner = _owner;
-	}
+    constructor(address initialOwner) {
+        transferOwnership(initialOwner);
+    }
 
-	// Modifier: used to define a set of rules that must be met before or after a function is executed
-	// Check the withdraw() function
-	modifier isOwner() {
-		// msg.sender: predefined variable that represents address of the account that called the current function
-		require(msg.sender == owner, "Not the Owner");
-		_;
-	}
+    receive() external payable {
+        emit EtherDeposited(msg.sender, msg.value);
+    }
 
-	/**
-	 * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
-	 *
-	 * @param _newGreeting (string memory) - new greeting to save on the contract
-	 */
-	function setGreeting(string memory _newGreeting) public payable {
-		// Print data to the hardhat chain console. Remove when deploying to a live network.
-		console.log(
-			"Setting new greeting '%s' from %s",
-			_newGreeting,
-			msg.sender
-		);
+    function sendMedia(uint256 _submissionId, bytes32 _mediaURIHash) external onlyOwner {
+        require(_mediaURIHash != 0, "Invalid media hash");
+        require(submissions[_submissionId].mediaURIHash == 0, "Submission ID already used");
 
-		// Change state variables
-		greeting = _newGreeting;
-		totalCounter += 1;
-		userGreetingCounter[msg.sender] += 1;
+        submissions[_submissionId] = Submission({
+            submitter: address(0),
+            mediaURIHash: _mediaURIHash,
+            eegDataHash: 0,
+            tokenReward: 0,
+            isPaid: false,
+            eegDataSubmitted: false
+        });
+        emit MediaSent(_submissionId, _mediaURIHash, msg.sender);
+    }
 
-		// msg.value: built-in global variable that represents the amount of ether sent with the transaction
-		if (msg.value > 0) {
-			premium = true;
-		} else {
-			premium = false;
-		}
+    function submitEEGData(uint256 _submissionId, bytes32 _eegDataHash) external {
+        Submission storage submission = submissions[_submissionId];
+        require(submission.mediaURIHash != 0, "Invalid submission ID");
+        require(!submission.eegDataSubmitted, "EEG data already submitted");
+        require(_eegDataHash != 0, "Invalid EEG data hash");
 
-		// emit: keyword used to trigger an event
-		emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, msg.value);
-	}
+        submission.submitter = msg.sender;
+        submission.eegDataHash = _eegDataHash;
+        submission.eegDataSubmitted = true;
+        emit EEGDataSubmitted(_submissionId, _eegDataHash);
+    }
 
-	/**
-	 * Function that allows the owner to withdraw all the Ether in the contract
-	 * The function can only be called by the owner of the contract as defined by the isOwner modifier
-	 */
-	function withdraw() public isOwner {
-		(bool success, ) = owner.call{ value: address(this).balance }("");
-		require(success, "Failed to send Ether");
-	}
+    function approveAndPay(uint256 _submissionId, address _submitterAddress) external onlyOwner nonReentrant {
+        Submission storage submission = submissions[_submissionId];
+        require(submission.submitter == _submitterAddress, "Submission ID and address do not match");
+        require(submission.submitter != address(0), "Submission not found");
+        require(!submission.isPaid, "Already paid");
+        require(submission.eegDataSubmitted, "EEG data not submitted");
 
-	/**
-	 * Function that allows the contract to receive ETH
-	 */
-	receive() external payable {}
+        submission.isPaid = true;
+        (bool sent, ) = submission.submitter.call{value: submission.tokenReward}("");
+        require(sent, "Failed to send Ether");
+
+        emit PaymentMade(_submissionId, submission.tokenReward);
+    }
+
+    function setReward(uint256 _submissionId, uint256 _reward) external onlyOwner {
+        Submission storage submission = submissions[_submissionId];
+        require(!submission.isPaid, "Already paid");
+        submission.tokenReward = _reward;
+    }
+
+    function withdrawEther(address payable _to, uint256 _amount) external onlyOwner nonReentrant {
+        require(_amount <= address(this).balance, "Insufficient balance");
+        (bool sent, ) = _to.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
 }
